c6adc9d239490553886a34d3de639a20b5f4e97b
Annemarleen Bosma <amholbosma@gmail.com>
2021-01-25 11:06:25 +0100

    new first commit that is almost ready to hand in

---
+++ Begin.py/begin.py
@@ -0,0 +1,172 @@
+#
+# Begin.py
+#
+# Opdracht: Tekstidentificatie
+#
+# Naam: Annemarleen Bosma
+#
+
+
+
+"""
+De methode __repr__(self) geeft een overzicht terug van alle dictionary’s in het model.
+Doel: zodat je ermee kan testen en kan controleren dat ze werken.
+> constructor en __repr__
+> read_text_from_file
+> make_sentence_lengths
+"""
+
+from string import punctuation
+
+
+class TextModel:
+    """A class supporting complex models of text."""
+
+    def __init__(self):
+        """
+        Constructor.
+        Create an empty TextModel.
+        """
+        
+        # Maak dictionary's voor elke eigenschap
+        
+        # attributes: 
+        self.words = {}             # Om woorden te tellen
+        self.word_lengths = {}      # Om woordlengtes te tellen
+        self.stems = {}             # Om stammen te tellen
+        self.sentence_lengths = {}  # Om zinslengtes te tellen
+        
+        # Maak een eigen dictionary:
+        
+        self.my_feature = {}        # Om ... te tellen
+
+        
+    def __repr__(self):
+        """
+        Display the contents of a TextModel.
+        """
+        s = 'Woorden:\n' + str(self.words) + '\n\n'
+        s += 'Woordlengtes:\n' + str(self.word_lengths) + '\n\n'
+        s += 'Stammen:\n' + str(self.stems) + '\n\n'
+        s += 'Zinslengtes:\n' + str(self.sentence_lengths) + '\n\n'
+        s += 'MIJN EIGENSCHAP:\n' + str(self.my_feature)
+        return s
+
+
+    # Voeg hier andere methodes toe.
+    # Je hebt in het bijzonder methodes nodig die het model vullen.
+
+    # METHODS
+    def read_text_from_file(self, filename):
+        """
+        methode:    read_text_from_file(self, filename) 
+        argument:   self; verwijst naar de klasse TextModel en maakt hiervan 'a instance of a class'
+        argument:   filename; van het type string. 
+                    self.text: variabele waaraan de inhoud toegekend is 
+                    van het bestand filename. Type: 1 hele lange string.   
+        """
+
+        with open("/home/annemarleen/programming/CS_for_all/Eindopdracht/test.txt") as f:
+            self.text = f.read().replace("\n", "").rstrip("")            
+
+        return self.text
+        
+
+    
+    def make_sentence_lengths(self, text):
+        """        
+        should use the text in the input string s to create the self.sentencelengths dictionary 
+
+        De make_sentence_lengths(self) moet parameter text (type string) gebruiken om de
+        dictionary self.sentence_lengths te vullen.
+        output: dictionary self.sentence_lengths.
+        Het resultaat van deze functie is: dictionary: sentence; words 
+
+        assert tm.sentence_lengths == {16: 1, 5: 1, 6: 1} => 1 zin met 16 woorden, 1 zin met 5 woorden, 1 zin met 6 woorden
+        
+
+        """
+        # dus, je moet het aantal zinnen tellen, én het aantal woorden in de zin.
+        # als x is zin, return True. If True, count words. if word endswhith '.?!' dan 
+
+        # count_words. If word endswith '.?!' dan return is_sentence. count_sentence =+ 1
+
+        # het resultaat hiervan wordt dmv een dictionary getoond.
+
+        print("The whole original string is:\n",self.text)
+        print("")
+       
+        # number of words:
+        count_words_text = 0
+        cutup_text = self.text.split() 
+
+        for word in cutup_text:
+            count_words_text +=1
+            print(word, count_words_text)
+            #print(count_words_text)
+            
+                   
+        total_num_words_text = len(cutup_text)
+        print("The number of words in the text are: ", str(total_num_words_text))
+
+
+        # NUMBER OF SENTENCES:
+        # print(cutup_text)        
+        sentence_count = 0
+        sentence = []
+        for w in cutup_text:
+            
+            if w[-1] not in ".?!":
+                sentence.append(w)
+                
+            #one_sentence = sentence.append(w)
+                       
+            if w[-1] in ".?!;":
+                last_word = []
+                sentence_count += 1
+                #one_sentence.append(last_word)
+                last_word.append(w)
+                print(f"the last word of the sentence was {last_word}.")
+                              
+                           
+            
+        print("The total number of sentences are: ", sentence_count)
+
+        # number of words in a sentence
+        self.sentence_lengths = {}
+        
+        # keys = range(len(cutup_text))
+        # values = [cutup_text]
+
+        # for i in keys:
+        #     self.sentence_lengths[i] = values[i]
+        # print(self.sentence_lengths)
+
+
+        pw = "$" # previous word
+        word_count = 0
+
+        for w in cutup_text:
+            
+            if pw not in self.sentence_lengths:
+                self.sentence_lengths[pw] = []
+            else:
+                self.sentence_lengths[pw] += [w]
+
+            if w[-1] in ".?!;":
+                pw = "$"
+            else:
+                pw = w
+
+        return self.sentence_lengths    
+     
+
+
+# _TESTS
+tm = TextModel()
+
+# Zet hier aanroepen neer die het model vullen met informatie
+print('TextModel:\n', tm)
+tm.read_text_from_file("/home/annemarleen/programming/CS_for_all/Eindopdracht/test.txt")
+
+#assert tm.text == test_text
